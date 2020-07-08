ec18c8a37ed3ca19c0346810e7709a68def9ff7d
Raghav Srinivaas M <63859534+srinivaas1998@users.noreply.github.com>
2020-07-08 17:45:46 +0530

    Update Aray_List.java

--- Aray_List.java
+++ Aray_List.java
@@ -1,87 +1,152 @@
-import java.util.Arrays;
 
+import java.util.Arrays;
 public class Array_List {
-	private Object[] obj;
-	private int arr_pos=0;
-	public Array_List(){
-		obj=new Object[10];
+	private Object[] array;
+	private Object[] temp_array;
+	private int arr_index=0;
+	public Array_List1(){
+		array=new Object[10];
 	}
 	
 	public void addArr(Object obj1) {
-		if(obj.length-arr_pos<=5) {
+		if(array.length-arr_index<=5) {
 			increaseArr();
 		}
-		obj[arr_pos++]=obj1;
+		array[arr_index++]=obj1;
 		
 	}
 	void increaseArr() {
-		obj=Arrays.copyOf(obj,obj.length*2);
+		temp_array=new Object[2*array.length];
+		for(int i=0;i<array.length;i++) {
+			temp_array[i]=array[i];
+		}
+		array=temp_array;
 	}
+	
 	int size() {
-		return arr_pos;
+		return arr_index;
 	}
 	public Object getArr(int index){
-        if(index < arr_pos){
-            return obj[index];
-        } else {
-            throw new ArrayIndexOutOfBoundsException();
-        }
-    }
+        	if(index < arr_index){
+            		return array[index];
+        	} else {
+            		throw new ArrayIndexOutOfBoundsException();
+        	}
+    	}
 	
 	 public Object removeArr(int index){
-	        if(index < arr_pos){
-	            Object obj2 = obj[index];
-	            obj[index] = null;
+	        if(index < arr_index){
+	            Object obj2 = array[index];
+	            array[index] = null;
 	            int tmp = index;
-	            while(tmp < arr_pos){
-	                obj[tmp] = obj[tmp+1];
-	                obj[tmp+1] = null;
+	            while(tmp < arr_index){
+	                array[tmp] = array[tmp+1];
+	                array[tmp+1] = null;
 	                tmp++;
 	            }
-	            arr_pos--;
-	            return obj;
+	            arr_index--;
+	            return array;
 	        } else {
 	            throw new ArrayIndexOutOfBoundsException();
 	        }
-	}
+	         
+	 }
+	 public void display() {
+		 for(int i=0;i<array.length;i++) {
+			 if(array[i]!=null) {
+				 System.out.println(array[i]);
+			 }
+		 }
+		 
+	 }
 	
 	public static void main(String[] args){
 	
-        Array_List al = new Array_List();
-        al.addArr(Integer.valueOf(2));
-        al.addArr(Integer.valueOf(3));
-        al.addArr(Integer.valueOf(5));
-        al.addArr(Integer.valueOf(4));
-        al.addArr(Integer.valueOf(7));
-        al.addArr(Integer.valueOf(2));
-        al.addArr(Integer.valueOf(7));
+	Array_List1 list = new Array_List1();
+        list.addArr(Integer.valueOf(2));
+        list.addArr(Integer.valueOf(3));
+        list.addArr(Integer.valueOf(5));
+        list.addArr(Integer.valueOf(4));
+        list.addArr(Integer.valueOf(7));
+        list.addArr(Integer.valueOf(2));
+        list.addArr(Integer.valueOf(67));
+        list.addArr(Integer.valueOf(57));
+        list.addArr(Integer.valueOf(72));
+        list.addArr(Integer.valueOf(74));
+        list.addArr(Integer.valueOf(74));
+        list.addArr(Integer.valueOf(17));
+        
+        
+        System.out.println("The elements in the array are:");
+        list.display();
+       
+        System.out.println("The size of the array is "+list.size());
         System.out.println("");
-        for(int i=0;i<al.size();i++){
-            System.out.print(al.getArr(i)+" ");
-        }
+        
+        list.addArr(Integer.valueOf(29));
+        System.out.println("The elements in the array after addition of new element is:");
+        list.display();
+        
         System.out.println("");
-        al.addArr(Integer.valueOf(29));
-        System.out.println("Element at Index 5:"+al.getArr(5));
+        System.out.println("Element at Index 5:"+list.getArr(5));
         System.out.println("");
-        for(int i=0;i<al.size();i++){
-            System.out.print(al.getArr(i)+" ");
-        }
-        System.out.println("");
-        al.removeArr(2);
-        for(int i=0;i<al.size();i++){
-            System.out.print(al.getArr(i)+" ");
-        }
+       
+        
+        list.removeArr(2);
+        System.out.println("The elements in the array after removal of an element is:");
+        list.display();
+      
         System.out.println("");
     }
+
+
 }
-
-
 //Output
 /*
-2 3 5 4 7 2 7                      //Displaying the array
-Element at Index 5:2               //Fetching element from position
+The elements in the array are:
+2
+3
+5
+4
+7
+2
+67
+57
+72
+74
+74
+17
+The size of the array is 12
 
-2 3 5 4 7 2 7 29                   //Addition of new Element
-2 3 4 7 2 7 29                     //Removing element
+The elements in the array after addition of new element is:
+2
+3
+5
+4
+7
+2
+67
+57
+72
+74
+74
+17
+29
+
+Element at Index 5:2
+
+The elements in the array after removal of an element is:
+2
+3
+4
+7
+2
+67
+57
+72
+74
+74
+17
+29
 
 */
